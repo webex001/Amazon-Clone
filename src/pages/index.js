40db09098114ec432e65f91c4554b268ea2890ad
David Bobboi Yakubu <55327588+webex001@users.noreply.github.com>
2022-02-26 21:36:32 +0100

    Banner section complete. working on the product section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
@@ -6,8 +7,13 @@ export default function Home() {
       <Head>
         <title>Amazon 2.0</title>
       </Head>
-      <h1>testing</h1>
+
+      <Header />
+
+      <main>
+        {/* Banner */}
+      </main>
       
-    </div> 
+    </div>  
   );
 }
